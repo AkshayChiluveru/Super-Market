f9a42929070c6d7602bce0c99a940fe0b3d44e97
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2023-07-04 10:54:14 +0530

    list of items to be displayed

---
+++ main.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+name = input("Enter Your Name: ")
+
+
+# List of Items: 
+lists = '''
+mangoes    Rs 50/each
+Chocolates Rs 20/each
+Rice       Rs 15/kg
+Colgate    Rs 10/each
+Oil        Rs 10/liter
+paneer     Rs 10/kg
+maggi      Rs 10/kg
+Complan    Rs 10/kg
+Chips      Rs 10/each
+cool DrinksRs 10/liter
+'''
